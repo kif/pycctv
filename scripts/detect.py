8fe15c19079a811ab3a8c83facac294c41bf206e
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2012-12-16 15:23:47 +0100

    more options

--- scripts/detect.py
+++ scripts/detect.py
@@ -11,10 +11,28 @@ __copyright__ = "Jérôme Kieffer"
 __date__ = "16/12/2012"
 __status__ = "devel"
 
-
+import optparse, logging, sys
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("cctv.detect")
 import cctv, cctv.image
 
 def main():
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-V", "--version", dest="version", action="store_true",
+                      help="print version of the program and quit", metavar="FILE", default=False)
+#        parser.add_option("-o", "--out", dest="outfile",
+#                          help="Filename where processed image is saved", metavar="FILE", default="merged.edf")
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="debug", default=False,
+                      help="switch to debug/verbose mode")
+    (options, args) = parser.parse_args()
+    if options.version:
+        print("python cctv detect version %s" % cctv.version)
+        sys.exit(0)
+    if options.debug:
+        logger.setLevel(logging.DEBUG)
+
 
     cctv.image.loop_delta()
 
